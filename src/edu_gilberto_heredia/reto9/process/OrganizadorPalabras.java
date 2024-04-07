7efe319155ab28f1669ebc3f3b900fd48d129c34
Gilberto Castro Heredia <AL02952503@tecmilenio.mx>
2024-04-06 19:07:58 -0600

    Reto 9. Casi acabado, errores por corregir, falta comentar y diagrama.

---
+++ src/edu_gilberto_heredia/reto9/process/OrganizadorPalabras.java
@@ -0,0 +1,11 @@
+package edu_gilberto_heredia.reto9.process;
+
+import java.util.*;
+
+public class OrganizadorPalabras {
+    public static List<Map.Entry<String, Integer>> organizarPalabras(Map<String, Integer> conteoPalabras) {
+        List<Map.Entry<String, Integer>> listaPalabras = new ArrayList<>(conteoPalabras.entrySet());
+        listaPalabras.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+        return listaPalabras;
+    }
+}
